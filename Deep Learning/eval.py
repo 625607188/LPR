95eaa4b6982c49432ed8720b0b73a1b07314f461
Hao <625607188@qq.com>
2018-02-28 21:27:39 +0800

    fix bugs

--- Deep Learning/eval.py
+++ Deep Learning/eval.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 from PIL import Image
 # 加载inference.py和train.py中定义的常量和函数。
 import CharacterRecognition
+import LicenseRecognition
 import inference
 import tfrecord
 
 
 def image_to_character(image_path):
-    img = cv2.imread(image_path, 0)
-    blur = img
-    ret3, image = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)          # 二值化处理
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    ret3, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)          # 二值化处理
     image = cv2.equalizeHist(image)                                                         # 均值化处理
 
     (x, y) = image.shape
@@ -64,7 +64,6 @@ def image_to_character(image_path):
             del para[section - i - 1]
             index = index + 1
     section = section - index
-
     return section, para
 
 
@@ -134,7 +133,7 @@ def evaluate_one_image(image_path):
             para[i] = tf.reshape(para[i], [20*20])
 
         x = tf.placeholder(
-            tf.float32, [None, inference.INPUT_NODE], name='x-input')
+            tf.float32, [None, CharacterRecognition.INPUT_NODE], name='x-input')
         # 直接使用inference.py中定义的前向传播过程。
         y_conv, keep_prob = CharacterRecognition.deepnn(x)
         y_soft = tf.nn.softmax(y_conv)
@@ -164,12 +163,60 @@ def evaluate_one_image(image_path):
     return result
 
 
-def main():
-    for i in range(1917):
-        path = "C:/Users/Hao/Desktop/test/" + str(i) + ".jpg"
-        image = evaluate_one_image(path)
+def eval(image_path):
+    path = "C:/Users/Hao/Desktop/temp/"
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    x_, y_, _ = image.shape
+    x_sum = 0
+    y_sum = 0
+    num = 0
+    with tf.Graph().as_default():
+        x = tf.placeholder(tf.float32, [None, LicenseRecognition.INPUT_NODE], name='x-input')
+        # 直接使用inference.py中定义的前向传播过程。
+        y_conv, keep_prob = LicenseRecognition.deepnn(x)
+        y_soft = tf.nn.softmax(y_conv)
+        pre = tf.argmax(y_soft, 1)
 
-        return image
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            print("Reading checkpoints...")
+            ckpt = tf.train.get_checkpoint_state(LicenseRecognition.MODEL_SAVE_PATH)
+            if ckpt and ckpt.model_checkpoint_path:
+                # 加载模型。
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                # 通过文件名得到模型保存时迭代的轮数。
+                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                print("Loading success, global_step is %s " % global_step)
+                for i in range(int((x_ - 120)/10)-44):
+                    for l in range(int((y_ - 480)/10)):
+                        img = image[((i+44)*10):(((i+44)*10) + 120), (l*10):((l*10) + 480)]
+                        img = cv2.resize(img, (40, 12), interpolation=cv2.INTER_AREA)
+                        img = np.array(img)
+                        img = tf.reshape(img, [40 * 12 * 3])
+
+                        xs = sess.run([img])
+                        prediction = int(pre.eval(feed_dict={x: xs, keep_prob: 1.0}))
+                        '''if '0' == tfrecord.license_classes[prediction]:
+                            print("x is %d, y is %d, pre is %d" % i, l, prediction)
+                        else:
+                            print("x is %d, y is %d, pre is %d" % i, l, prediction)'''
+                        print("x is %d, y is %d, pre is %d" % (i, l, prediction))
+                        if '0' == tfrecord.license_classes[prediction]:
+                            num += 1
+                            x_sum += (i+44)*10
+                            y_sum += (l*10)
+
+                x_sum = int(x_sum/num)
+                y_sum = int(y_sum/num)
+                cv2.imwrite(path + "0.jpg", image[x_sum:(x_sum + 120), y_sum:(y_sum + 480)])
+            else:
+                print('No checkpoint file found')
+
+
+def main(_):
+    #evaluate_one_image("D:/final work/FinalWork-Ms.Wu/Project/Train/svm/has/test/0.jpg")
+    eval("C:/Users/Hao/Desktop/temp/test.jpg")
 
 
 if __name__ == '__main__':
